e4a1a5e16b838ce96fe001968c6467acc9a57d5b
Christopher Oliveira <chrisoliveira1787@gmail.com>
2019-05-29 09:12:29 -0400

    Updates

---
+++ precache-manifest.ef0c665d51e8b41398361bce1dd71948.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c664f6f3b143a3aa69e0378ef832283e",
+    "url": "/memory-test/index.html"
+  },
+  {
+    "revision": "b0538ba1e6289d2a55cf",
+    "url": "/memory-test/static/css/main.7bdd20e0.chunk.css"
+  },
+  {
+    "revision": "7572a71b78fe9a86660e",
+    "url": "/memory-test/static/js/2.0bc8cb03.chunk.js"
+  },
+  {
+    "revision": "b0538ba1e6289d2a55cf",
+    "url": "/memory-test/static/js/main.192d9b89.chunk.js"
+  },
+  {
+    "revision": "18f364b1dc4c2b2d0e5c",
+    "url": "/memory-test/static/js/runtime~main.c31bef12.js"
+  }
+]);
